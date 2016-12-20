783f48d42b7f097d156d492fc9ab1f17cb006756
Nelson R. Perez <bilthon@gmail.com>
2016-12-19 22:05:04 -0500

    Defining get_trade_history in RPC constants class

--- src/main/java/de/bitsharesmunich/graphenej/api/GetTradeHistory.java
+++ src/main/java/de/bitsharesmunich/graphenej/api/GetTradeHistory.java
@@ -49,7 +49,7 @@ public class GetTradeHistory extends WebSocketAdapter {
         accountParams.add(this.fromTime);
         accountParams.add(this.limit);
 
-        ApiCall getAccountByName = new ApiCall(0, "get_trade_history", accountParams, RPC.VERSION, 1);
+        ApiCall getAccountByName = new ApiCall(0, RPC.GET_TRADE_HISTORY, accountParams, RPC.VERSION, 1);
         websocket.sendText(getAccountByName.toJsonString());
     }
 
