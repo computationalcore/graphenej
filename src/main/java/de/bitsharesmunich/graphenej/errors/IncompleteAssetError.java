e4768c700ccbe0d8ab1bb2b9f9fa69a244912a92
Nelson R. Perez <bilthon@gmail.com>
2017-01-10 14:23:44 -0500

    Added new constructor

--- src/main/java/de/bitsharesmunich/graphenej/errors/IncompleteAssetError.java
+++ src/main/java/de/bitsharesmunich/graphenej/errors/IncompleteAssetError.java
@@ -4,6 +4,11 @@ package de.bitsharesmunich.graphenej.errors;
  * Created by nelson on 12/25/16.
  */
 public class IncompleteAssetError extends RuntimeException{
+
+    public IncompleteAssetError(String message){
+        super(message);
+    }
+
     public IncompleteAssetError(){
         super("The asset used in this method is probably incomplete, Assets instances can be created with just its id information but this context requires more information");
     }
