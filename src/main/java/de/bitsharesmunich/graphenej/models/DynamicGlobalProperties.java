1eb190c07ef8e894c1cde8024cd7f6754910c6b8
Nelson R. Perez <bilthon@gmail.com>
2017-01-18 22:34:02 -0500

    Extending the DynamicGlobalProperties class

--- src/main/java/de/bitsharesmunich/graphenej/models/DynamicGlobalProperties.java
+++ src/main/java/de/bitsharesmunich/graphenej/models/DynamicGlobalProperties.java
@@ -1,11 +1,28 @@
 package de.bitsharesmunich.graphenej.models;
 
+import java.io.Serializable;
+
+import de.bitsharesmunich.graphenej.GrapheneObject;
+
 /**
  * Class used to deserialize the 'result' field returned by the full node after making a call
  * to the 'get_dynamic_global_properties' RPC.
  */
-public class DynamicGlobalProperties {
-    public String id;
+public class DynamicGlobalProperties extends GrapheneObject implements Serializable {
+    public static final String KEY_HEAD_BLOCK_NUMBER = "head_block_number";
+    public static final String KEY_HEAD_BLOCK_ID ="head_block_id";
+    public static final String KEY_TIME = "time";
+    public static final String KEY_CURRENT_WITNESS = "current_witness";
+    public static final String KEY_NEXT_MAINTENANCE_TIME = "next_maintenance_time";
+    public static final String KEY_LAST_BUDGET_TIME = "last_budget_time";
+    public static final String KEY_WITNESS_BUDGET = "witness_budget";
+    public static final String KEY_ACCOUNTS_REGISTERED_THIS_INTERVAL = "accounts_registered_this_interval";
+    public static final String KEY_RECENTLY_MISSED_COUNT = "recently_missed_count";
+    public static final String KEY_CURRENT_ASLOT = "current_aslot";
+    public static final String KEY_RECENT_SLOTS_FILLED = "recent_slots_filled";
+    public static final String KEY_DYNAMIC_FLAGS = "dynamic_flags";
+    public static final String KEY_LAST_IRREVERSIBLE_BLOCK_NUM = "last_irreversible_block_num";
+
     public long head_block_number;
     public String head_block_id;
     public String time;
@@ -19,4 +36,8 @@ public class DynamicGlobalProperties {
     public String recent_slots_filled;
     public long dynamic_flags;
     public long last_irreversible_block_num;
+
+    public DynamicGlobalProperties(String id) {
+        super(id);
+    }
 }
