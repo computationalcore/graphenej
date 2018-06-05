40222055aa7b47cf2a4b0c204ba9268025fdb73c
Nelson R. Perez <bilthon@gmail.com>
2018-06-04 22:10:55 -0500

    Including usage of the GetAccounts API call using the single service conection in the sample app

--- sample/src/main/java/com/luminiasoft/labs/sample/MainActivity.java
+++ sample/src/main/java/com/luminiasoft/labs/sample/MainActivity.java
@@ -13,15 +13,20 @@ import android.widget.TextView;
 
 import com.google.gson.Gson;
 
+import java.util.HashMap;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cy.agorise.graphenej.UserAccount;
 import cy.agorise.graphenej.api.ConnectionStatusUpdate;
 import cy.agorise.graphenej.api.android.NetworkService;
 import cy.agorise.graphenej.api.android.RxBus;
+import cy.agorise.graphenej.api.calls.GetAccounts;
 import cy.agorise.graphenej.api.calls.GetBlock;
 import cy.agorise.graphenej.models.JsonRpcResponse;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,13 +43,20 @@ public class MainActivity extends AppCompatActivity {
 
     private Gson gson = new Gson();
 
+    private Disposable mDisposable;
+
+    private HashMap<Integer, Integer> responseMap = new HashMap<>();
+
+    private final int GET_BLOCK_RESPONSE = 0;
+    private final int GET_ACCOUNTS_RESPONSE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        RxBus.getBusInstance()
+        mDisposable = RxBus.getBusInstance()
             .asFlowable()
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(new Consumer<Object>() {
@@ -58,16 +70,53 @@ public class MainActivity extends AppCompatActivity {
                     Log.d(TAG,"Got connection update. Status: "+((ConnectionStatusUpdate)message).getConnectionStatus());
                     mConnectionStatus.setText(((ConnectionStatusUpdate) message).getConnectionStatus());
                 }else if(message instanceof JsonRpcResponse){
-                    mResponse.setText(mResponse.getText() + gson.toJson(message, JsonRpcResponse.class) + "\n");
+                    handleJsonRpcResponse((JsonRpcResponse) message);
                 }
             }
         });
     }
 
-    @OnClick(R.id.send_message)
-    public void onSendMesage(View v){
+    /**
+     * Internal method that will decide what to do with each JSON-RPC response
+     *
+     * @param response The JSON-RPC api call response
+     */
+    private void handleJsonRpcResponse(JsonRpcResponse response){
+        int id = (int) response.id;
+        if(responseMap.get(id) != null){
+            int responseId = responseMap.get(id);
+            switch(responseId){
+                case GET_BLOCK_RESPONSE:
+                    mResponse.setText(mResponse.getText() + gson.toJson(response, JsonRpcResponse.class) + "\n");
+                    break;
+                case GET_ACCOUNTS_RESPONSE:
+                    mResponse.setText(mResponse.getText() + gson.toJson(response, JsonRpcResponse.class) + "\n");
+                    break;
+                default:
+                    Log.w(TAG,"Case not handled");
+            }
+            // Remember to remove the used id entry from the map, as it would
+            // otherwise just increase the app's memory usage
+            responseMap.remove(id);
+        }else{
+            mResponse.setText(mResponse.getText() + gson.toJson(response, JsonRpcResponse.class) + "\n");
+        }
+    }
+
+    @OnClick(R.id.call_get_block)
+    public void onGetBlock(View v){
         GetBlock getBlock = new GetBlock(1000000);
-        mService.sendMessage(getBlock, GetBlock.REQUIRED_API);
+        int id = mService.sendMessage(getBlock, GetBlock.REQUIRED_API);
+        // Registering the used sequence id
+        responseMap.put(id, GET_BLOCK_RESPONSE);
+    }
+
+    @OnClick(R.id.call_get_accounts)
+    public void onGetAccounts(View v){
+        GetAccounts getAccounts = new GetAccounts(new UserAccount("1.2.1000"));
+        int id = mService.sendMessage(getAccounts, GetBlock.REQUIRED_API);
+        // Registering the used sequence id
+        responseMap.put(id, GET_ACCOUNTS_RESPONSE);
     }
 
     @OnClick(R.id.next_activity)
@@ -88,6 +137,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         unbindService(mConnection);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDisposable.dispose();
     }
 
     /** Defines callbacks for backend binding, passed to bindService() */
